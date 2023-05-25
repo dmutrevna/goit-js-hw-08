dd2b50e4173e3df978db24816a5ec5773dcd093d
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-05-25 07:42:46 +0200

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+player.on('ended', () => {
+  localStorage.removeItem('videoplayer-current-time');
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const { form, email, textarea } = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onButtonSubmit);
+
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: textarea.value,
+  };
+
+  saveFormData(formData);
+}
+
+function saveFormData(formData) {
+  const throttledSaveMsg = throttle(() => {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500);
+
+  throttledSaveMsg();
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+
+  if (savedData) {
+    const { email: savedEmail, message: savedMessage } = JSON.parse(savedData);
+    email.value = savedEmail;
+    textarea.value = savedMessage;
+  }
+}
+
+loadFormData();
+
+function onButtonSubmit(e) {
+  e.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+
+  const formData = {
+    email: email.value,
+    message: textarea.value,
+  };
+
+  console.log(formData);
+}
